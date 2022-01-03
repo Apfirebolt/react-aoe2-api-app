8564f515cc2c267142c92e9a5ca2c342338e9021
Amit P <aspper20@gmail.com>
2022-01-04 00:49:28 +0530

    added header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,25 @@
 import React, { Fragment } from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const FooterComponent = () => {
   return (
     <Fragment>
-      <footer className="container">
-        <div className="columns">
-          <div className="column col-12 justify-center mt-3">
-            <p>Copyright @2022 Star Wars API</p>
-          </div>
-        </div>
-      </footer>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            padding: "1rem",
+            display: "flex",
+            backgroundColor: "#ffef96",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h6" gutterBottom component="div" color="#50394c">
+            Age of Empires 2 API, all rights reserved @2022
+          </Typography>
+        </Box>
+      </Container>
     </Fragment>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+import Typography from '@mui/material/Typography';
+
 const HeaderComponent = () => {
   return (
-    <nav>
-      <p className="brand">Star Wars API</p>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/civilizations">Civilizations</Link>
-        </li>
-        <li>
-          <Link to="/units">Units</Link>
-        </li>
-        <li>
-          <Link to="/technologies">Technologies</Link>
-        </li>
-        <li>
-          <Link to="/structures">Structures</Link>
-        </li>
-      </ul>
-    </nav>
+    <Container maxWidth="lg">
+      <Box sx={{ 
+        bgcolor: "#cfe8fc", 
+        height: "auto", 
+        padding: "1rem", 
+        display: 'flex', 
+        justifyContent: 'space-between',
+        alignItems: 'center'
+        }}>
+        <PopupState variant="popover" popupId="demo-popup-menu">
+          {(popupState) => (
+            <React.Fragment>
+              <Typography variant="h4" gutterBottom component="div">
+        Age of Empires 2 API
+      </Typography>
+              <Button variant="contained" {...bindTrigger(popupState)}>
+                Menu
+              </Button>
+              <Menu {...bindMenu(popupState)}>
+                <MenuItem onClick={popupState.close}>
+                  <Link to="/">Home</Link>
+                </MenuItem>
+                <MenuItem onClick={popupState.close}>
+                  <Link to="/civilizations">Civilizations</Link>
+                </MenuItem>
+                <MenuItem onClick={popupState.close}>
+                  <Link to="/units">Units</Link>
+                </MenuItem>
+                <MenuItem onClick={popupState.close}>
+                  <Link to="/technologies">Technologies</Link>
+                </MenuItem>
+                <MenuItem onClick={popupState.close}>
+                  <Link to="/structures">Structures</Link>
+                </MenuItem>
+              </Menu>
+            </React.Fragment>
+          )}
+        </PopupState>
+      </Box>
+    </Container>
   );
 };
 
